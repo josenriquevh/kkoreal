c66a847517919983dd0ca47bb8d46cbb0ca556fa
Jose Enrique <josenriquev@gmail.com>
2016-02-21 18:38:15 -0430

    Probando subiendo archivo

---
+++ pg_conection.php
@@ -0,0 +1,8 @@
+<?php
+
+pg_connect("host=localhost dbname=anviz user=postgres password=1234")
+    or die("No se puede conectar a la base de datos. ".pg_last_error());
+
+    echo "se conecto.";
+
+?>
