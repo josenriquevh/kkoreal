7c3feb0f702a67fd33f49081501a7f96a58e5cbb
Jose Velasquez <josenriquev@gmail.com>
2016-02-21 19:18:40 -0430

    Actualizando 2

--- pg_conection.php
+++ pg_conection.php
@@ -3,6 +3,6 @@
 pg_connect("host=localhost dbname=anviz user=postgres password=1234")
     or die("No se puede conectar a la base de datos. ".pg_last_error());
 
-    echo "se conecto. Actualizando";
+    echo "se conecto. Actualizando 2";
 
 ?>
